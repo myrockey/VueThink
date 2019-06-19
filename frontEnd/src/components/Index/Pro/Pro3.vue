82557520b9fafb4315fa8dd24b6e4a3047ee836d
rockey <2771452556@qq.com>
2019-06-19 16:27:42 +0800

    添加二维码

--- frontEnd/src/components/Index/Pro/Pro3.vue
+++ frontEnd/src/components/Index/Pro/Pro3.vue
@@ -16,7 +16,7 @@
                         <div class="pro3-btn-box">
                         <div class="hs-container">
                             <el-row class="fr m-l-30">
-                                <img width="132" height="132" :src="imgQrData.src" :alt="imgQrData.title"/>
+                                <img width="132" height="132" :src="imgQr2Data.src" :alt="imgQr2Data.title"/>
                                 <div>客服微信二维码</div>
                             </el-row>
                             <el-row class="fr">
@@ -49,6 +49,7 @@
           { src: require('../../../assets/images/pro3.jpg'), title: '城市之芯', link: 'Pro3' }
         ],
         imgQrData: { src: require('../../../assets/images/pro3_qr.png'), title: '周期研习社' },
+        imgQr2Data: { src: require('../../../assets/images/pro3_qr2.png'), title: '周期研习社' },
         imgPro3Data: { src: require('../../../assets/images/pro3_01.png'), title: '周期研习社' },
         imgPro3Data2: { src: require('../../../assets/images/pro3_02.png'), title: '周期研习社' }
       }
